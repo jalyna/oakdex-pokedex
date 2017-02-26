7715cd7e6abc2a14d2eb28110e0cccfb5c1dd504
Jalyna <jalyna.schroeder@gmail.com>
2017-02-26 21:47:05 +0100

    Add pokedex types

---
+++ spec/schema_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'json-schema'
+
+describe 'types' do
+  Dir['data/types/*.json'].each do |file_name|
+    it "#{file_name} follows schema" do
+      expect(JSON::Validator.validate('data/schemas/type.json', file_name)).to be(true)
+    end
+  end
+end
