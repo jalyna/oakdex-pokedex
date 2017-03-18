864cd966b3df5bf38c68db613772c994f6368b45
Jalyna <jalyna.schroeder@gmail.com>
2017-03-18 15:14:42 +0100

    add findPokemon

--- src/oakdex_pokedex.js
+++ src/oakdex_pokedex.js
@@ -1,10 +1,43 @@
 var fs = require('fs');
 
+var allByName = function(type, cb) {
+  if(!this.byName) {
+    this.byName = {};
+  }
+  if(this.byName[type]) {
+    cb(this.byName[type]);
+    return;
+  }
+  var _this = this;
+  _this.byName[type] = {};
+  fs.readdir('./data/' + type, function(err, filenames) {
+    filenames = filenames.filter(function(filename) {
+      return filename.includes('.json');
+    });
+    filenames.forEach(function(filename) {
+      fs.readFile('./data/' + type + '/' + filename, function (err, data) {
+        if(err) {
+          throw err;
+        }
+        var obj = JSON.parse(data);
+        _this.byName[type][obj.names.en] = obj;
+        if(Object.keys(_this.byName[type]).length == filenames.length) {
+          cb(_this.byName[type]);
+        }
+      });
+    });
+  });
+};
+
 module.exports = {
 
-  getPokemon: function(cb) {
-    fs.readFile('./data/pokemon/bulbasaur.json', 'utf8', function (err, data) {
-      var obj = JSON.parse(data);
+  findPokemon: function(idOrName, cb) {
+    allByName('pokemon', function(list) {
+      obj = list[idOrName];
+      if(!obj) {
+        cb(null);
+        return;
+      }
       cb(obj);
     });
   }
--- test/test-oakdex-pokedex.js
+++ test/test-oakdex-pokedex.js
@@ -1,11 +1,25 @@
 var oakdexPokedex = require('../src/oakdex_pokedex');
 var expect = require('expect.js');
+var fs = require('fs');
 
 describe('OakdexPokedex', function() {
-  it('#getPokemon', function(done) {
-    oakdexPokedex.getPokemon(function(pkmn) {
-      expect(pkmn.names.de).to.equal('Bisasam');
-      done();
+  describe('#findPokemon', function() {
+    it('finds eevee', function(done) {
+      oakdexPokedex.findPokemon('Eevee', function(pkmn) {
+        fs.readFile('./data/pokemon/eevee.json', 'utf8', function (err, data) {
+          var obj = JSON.parse(data);
+          expect(pkmn.names.de).to.equal('Evoli');
+          expect(pkmn).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if pokemon does not exist', function(done) {
+      oakdexPokedex.findPokemon('NotKnownByMe', function(pkmn) {
+        expect(pkmn).to.equal(null);
+        done();
+      });
     });
   });
 });
