b634f8817171efff282ff1e347fdde16f5044738
Jalyna <jalyna.schroeder@gmail.com>
2017-03-18 16:19:30 +0100

    Add NPM package (#46)
    * Setup karma jasmine grund phantom
    
    * Use mocha
    
    * add findPokemon
    
    * find pokemon by id
    
    * Add find methods
    
    * fix spec
    
    * enable eslint in codeclimate
    
    * Fix eslint issues
    
    * add all methods
    
    * improve specs
    
    * change signature of all methods
    
    * Add test to npmignore

---
+++ src/oakdex_pokedex.js
@@ -0,0 +1,174 @@
+var fs = require('fs');
+
+var allByName = function(type, cb) {
+  if(!this.byName) {
+    this.byName = {};
+  }
+  if(this.byName[type]) {
+    cb(this.byName[type]);
+    return;
+  }
+  var _this = this;
+  _this.byName[type] = {};
+  fs.readdir('./data/' + type, function(err, filenames) {
+    if(err) {
+      throw err;
+    }
+    filenames = filenames.filter(function(filename) {
+      return filename.indexOf('.json') !== -1;
+    });
+    filenames.forEach(function(filename) {
+      fs.readFile('./data/' + type + '/' + filename, function (err, data) {
+        if(err) {
+          throw err;
+        }
+        var obj = JSON.parse(data);
+        _this.byName[type][obj.names.en] = obj;
+        if(Object.keys(_this.byName[type]).length === filenames.length) {
+          return cb(_this.byName[type]);
+        }
+      });
+    });
+  });
+};
+
+var pokemonById = function(cb) {
+  var _this = this;
+  if(_this.pokemonByIdList) {
+    cb(_this.pokemonByIdList);
+    return;
+  }
+  _this.pokemonByIdList = {};
+  allByName('pokemon', function(list) {
+    Object.keys(list).map(function(key, _index) {
+      _this.pokemonByIdList[list[key].national_id] = list[key];
+    });
+    cb(_this.pokemonByIdList);
+  });
+};
+
+var findByType = function(type, name, cb) {
+  allByName(type, function(list) {
+    var obj = list[name];
+    if(!obj) {
+      cb(null);
+      return;
+    }
+    cb(obj);
+  });
+};
+
+var allByType = function(type, conditions, cb) {
+  if (typeof conditions === 'function') {
+    cb = conditions;
+    conditions = {};
+  }
+  allByName(type, function(list) {
+    var objects = [];
+    Object.keys(list).map(function(key) {
+      var item = list[key];
+      if(Object.keys(conditions).length === 0) {
+        objects.push(item);
+      } else {
+        var exists = false;
+        Object.keys(conditions).map(function(attr) {
+          var value = conditions[attr];
+          if(attr === 'dex') {
+            if(item[value + '_id']) {
+              objects.push(item);
+            }
+          } else if(item[attr] && item[attr].constructor === Array) {
+            if(item[attr].indexOf(value) > -1) {
+              objects.push(item);
+            }
+          } else {
+            if(item[attr] === value) {
+              objects.push(item);
+            }
+          }
+        });
+      }
+    });
+    cb(objects);
+  });
+};
+
+module.exports = {
+
+  findPokemon: function(idOrName, cb) {
+    allByName('pokemon', function(list) {
+      var obj = list[idOrName];
+      if(!obj) {
+        pokemonById(function(listId) {
+          obj = listId[idOrName];
+          if(!obj) {
+            cb(null);
+            return;
+          }
+          cb(obj);
+        });
+        return;
+      }
+      cb(obj);
+    });
+  },
+
+  findMove: function(name, cb) {
+    findByType('move', name, cb);
+  },
+
+  findAbility: function(name, cb) {
+    findByType('ability', name, cb);
+  },
+
+  findType: function(name, cb) {
+    findByType('type', name, cb);
+  },
+
+  findEggGroup: function(name, cb) {
+    findByType('egg_group', name, cb);
+  },
+
+  findGeneration: function(name, cb) {
+    findByType('generation', name, cb);
+  },
+
+  findNature: function(name, cb) {
+    findByType('nature', name, cb);
+  },
+
+  allPokemon: function(conditions, cb) {
+    if(conditions.type) {
+      conditions.types = conditions.type;
+    }
+    if(conditions.egg_group) {
+      conditions.egg_groups = conditions.egg_group;
+    }
+    allByType('pokemon', conditions, cb);
+  },
+
+  allMoves: function(conditions, cb) {
+    allByType('move', conditions, cb);
+  },
+
+  allAbilities: function(conditions, cb) {
+    allByType('ability', conditions, cb);
+  },
+
+  allTypes: function(conditions, cb) {
+    allByType('type', conditions, cb);
+  },
+
+  allEggGroups: function(conditions, cb) {
+    allByType('egg_group', conditions, cb);
+  },
+
+  allGenerations: function(conditions, cb) {
+    allByType('generation', conditions, cb);
+  },
+
+  allNatures: function(conditions, cb) {
+    allByType('nature', conditions, cb);
+  }
+
+};
---
+++ test/test-oakdex-pokedex.js
@@ -0,0 +1,265 @@
+var oakdexPokedex = require('../src/oakdex_pokedex');
+var expect = require('expect.js');
+var fs = require('fs');
+
+describe('OakdexPokedex', function() {
+  describe('#findPokemon', function() {
+    it('finds eevee by name', function(done) {
+      oakdexPokedex.findPokemon('Eevee', function(pkmn) {
+        fs.readFile('./data/pokemon/eevee.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(pkmn.names.de).to.equal('Evoli');
+          expect(pkmn).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('finds charmander by id', function(done) {
+      oakdexPokedex.findPokemon(4, function(pkmn) {
+        fs.readFile('./data/pokemon/charmander.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(pkmn.names.de).to.equal('Glumanda');
+          expect(pkmn).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if pokemon does not exist', function(done) {
+      oakdexPokedex.findPokemon('NotKnownByMe', function(pkmn) {
+        expect(pkmn).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#findMove', function() {
+    it('finds Tackle by name', function(done) {
+      oakdexPokedex.findMove('Tackle', function(found) {
+        fs.readFile('./data/move/tackle.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(found).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if entry does not exist', function(done) {
+      oakdexPokedex.findMove('NotKnownByMe', function(found) {
+        expect(found).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#findAbility', function() {
+    it('finds Contrary by name', function(done) {
+      oakdexPokedex.findAbility('Contrary', function(found) {
+        fs.readFile('./data/ability/contrary.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(found).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if entry does not exist', function(done) {
+      oakdexPokedex.findAbility('NotKnownByMe', function(found) {
+        expect(found).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#findType', function() {
+    it('finds Fairy by name', function(done) {
+      oakdexPokedex.findType('Fairy', function(found) {
+        fs.readFile('./data/type/fairy.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(found).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if entry does not exist', function(done) {
+      oakdexPokedex.findType('NotKnownByMe', function(found) {
+        expect(found).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#findEggGroup', function() {
+    it('finds Water 1 by name', function(done) {
+      oakdexPokedex.findEggGroup('Water 1', function(found) {
+        fs.readFile('./data/egg_group/water_1.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(found).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if entry does not exist', function(done) {
+      oakdexPokedex.findEggGroup('NotKnownByMe', function(found) {
+        expect(found).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#findGeneration', function() {
+    it('finds Generation VI by name', function(done) {
+      oakdexPokedex.findGeneration('Generation VI', function(found) {
+        fs.readFile('./data/generation/6.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(found).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if entry does not exist', function(done) {
+      oakdexPokedex.findGeneration('NotKnownByMe', function(found) {
+        expect(found).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#findNature', function() {
+    it('finds Bold by name', function(done) {
+      oakdexPokedex.findNature('Bold', function(found) {
+        fs.readFile('./data/nature/bold.json', 'utf8', function (err, data) {
+          if(err) {
+            throw err;
+          }
+          var obj = JSON.parse(data);
+          expect(found).to.eql(obj);
+          done();
+        });
+      });
+    });
+
+    it('returns null if entry does not exist', function(done) {
+      oakdexPokedex.findNature('NotKnownByMe', function(found) {
+        expect(found).to.equal(null);
+        done();
+      });
+    });
+  });
+
+  describe('#allPokemon', function() {
+    it('finds all pokemon', function(done) {
+      oakdexPokedex.allPokemon(function(list) {
+        expect(list.length).to.equal(802);
+        done();
+      });
+    });
+
+    it('finds Dark pokemon', function(done) {
+      oakdexPokedex.allPokemon({ type: 'Dark' }, function(list) {
+        expect(list.length).to.equal(46);
+        done();
+      });
+    });
+
+    it('finds Human-Like pokemon', function(done) {
+      oakdexPokedex.allPokemon({ egg_group: 'Human-Like' }, function(list) {
+        expect(list.length).to.equal(52);
+        done();
+      });
+    });
+
+    it('finds by alola id', function(done) {
+      oakdexPokedex.allPokemon({ dex: 'alola' }, function(list) {
+        expect(list.length).to.equal(302);
+        done();
+      });
+    });
+  });
+
+  describe('#allMoves', function() {
+    it('finds all moves', function(done) {
+      oakdexPokedex.allMoves(function(list) {
+        expect(list.length).to.equal(701);
+        done();
+      });
+    });
+
+    it('finds Ground moves', function(done) {
+      oakdexPokedex.allMoves({ type: 'Ground' }, function(list) {
+        expect(list.length).to.equal(26);
+        done();
+      });
+    });
+  });
+
+  describe('#allAbilities', function() {
+    it('finds all abilities', function(done) {
+      oakdexPokedex.allAbilities(function(list) {
+        expect(list.length).to.equal(232);
+        done();
+      });
+    });
+  });
+
+  describe('#allTypes', function() {
+    it('finds all types', function(done) {
+      oakdexPokedex.allTypes(function(list) {
+        expect(list.length).to.equal(18);
+        done();
+      });
+    });
+  });
+
+  describe('#allEggGroups', function() {
+    it('finds all egg groups', function(done) {
+      oakdexPokedex.allEggGroups(function(list) {
+        expect(list.length).to.equal(15);
+        done();
+      });
+    });
+  });
+
+  describe('#allGenerations', function() {
+    it('finds all generations', function(done) {
+      oakdexPokedex.allGenerations(function(list) {
+        expect(list.length).to.equal(7);
+        done();
+      });
+    });
+  });
+
+  describe('#allNatures', function() {
+    it('finds all natures', function(done) {
+      oakdexPokedex.allNatures(function(list) {
+        expect(list.length).to.equal(25);
+        done();
+      });
+    });
+  });
+});
